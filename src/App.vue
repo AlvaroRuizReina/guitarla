e3885a4402718e5c250f8bf84f83ba39f9e8a826
AlvaroRuizReina <alvaroruizreina90@gmail.com>
2025-03-21 15:09:44 +0100

    feat: avoid duplicate entries in the shopping cart

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,13 @@ onMounted(() => {
 
 // Method Event "agregarCarrito"
 const agregarCarrito = (guitarra) => {
-    guitarra.cantidad = 1
-    carrito.value.push(guitarra)
+    const existeCarrito = carrito.value.findIndex(producto => producto.id === guitarra.id)
+    if (existeCarrito >= 0) {
+        carrito.value[existeCarrito].cantidad++
+    } else {
+        guitarra.cantidad = 1
+        carrito.value.push(guitarra)
+    }
 }
 </script>
 
